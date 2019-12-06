e0399274066f9253cdec66c3332a4ddf2ea5eb16
Evgeniy Roschin <evgeny689@yandex.ru>
2019-12-06 14:18:24 +0400

    corrects

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 import checkCount from '../app';
 
 test('Number', () => {
@@ -10,4 +11,16 @@ test('String Number', () => {
 
 test('String', () => {
   expect(() => checkCount('www')).toThrowError('www не является числом');
+});
+
+test('Number,number', () => {
+  expect(() => checkCount(10.35)).toThrowError('10.35 не является числом');
+});
+
+test('Number + string', () => {
+  expect(() => checkCount('10dbbg')).toThrowError('10dbbg не является числом');
+});
+
+test('Number + sumbol', () => {
+  expect(() => checkCount('10!')).toThrowError('10! не является числом');
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-console */
+/* eslint-disable use-isnan */
+/* eslint-disable consistent-return */
 // // TODO: write your code here
 // import sum from './basic';
 
@@ -5,12 +8,19 @@
 
 // console.log(sum([1, 2]));
 
+// export default function checkCount(count) {
+//   try {
+//     if (Number(count) !== NaN) {
+//       return Number(count);
+//     }
+//   } catch (e) {
+//     console.log(`${count} не является числом`);
+//   }
+// }
+
 export default function checkCount(count) {
-  try {
-    if (Number(count) !== NaN) {
-      return Number(count);
-    }
-  } catch (e) {
-    console.log(`${count} не является числом`);
+  if (!Number.isInteger(Number(count))) {
+    throw new Error(`${count} не является числом`);
   }
+  return Number(count);
 }
